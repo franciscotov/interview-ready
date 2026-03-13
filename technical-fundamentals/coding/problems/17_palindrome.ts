ecdb2117ef80f7259a6f6b48d72a24ba75da547c
franciscodejesus.tovarceballo.contractor.gdevtools.com <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2026-03-12 22:40:08 -0300

    adding solution for is palindrome

--- technical-fundamentals/coding/problems/17_palindrome.ts
+++ technical-fundamentals/coding/problems/17_palindrome.ts
@@ -9,4 +9,25 @@ export type Node<T> = {
   next?: Node<T>;
 };
 
-export default function isPalindrome<T>(head: Node<T> | undefined): boolean {}
+export default function isPalindrome<T>(head: Node<T> | undefined): boolean {
+  let list = new LinkedList(head);
+  let map = new Map<T, number>();
+  let str = '';
+  let l = list.length();
+  if(l === 1) return true;
+  let even = l%2 === 0;
+  let pivot = Math.floor(l/2);
+  let arr: T[] = [];
+  let isPal = true;
+  list.visit((node, i) => {
+    console.log({pivot, l})
+    if(i< pivot) {
+      arr.push(node.value);
+    } else if (!even && i === pivot+1) {
+      // continue
+    } else {
+      isPal = arr.pop() === node.value;
+    }
+  })
+  return isPal;
+}
