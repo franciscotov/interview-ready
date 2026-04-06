bb5ca2358d73ff0632c70f86735bc01661cf7bfe
franciscodejesus.tovarceballo.contractor.gdevtools.com <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2026-04-06 12:02:11 -0300

    adding solution for three and min stacks

--- technical-fundamentals/coding/problems/21_threeStacks.ts
+++ technical-fundamentals/coding/problems/21_threeStacks.ts
@@ -1,17 +1,54 @@
 // 1. *Three in One*: Describe how you could use a single array to implement three stacks.
 
 export default class ThreeStacks<T> {
-    private array: T[];
+    private array: Array<T | undefined>//T[];
+    private stackCapacity: number;
 
     constructor(arrayLength: number) {
+        this.array = new Array<T>(arrayLength);
+        this.stackCapacity = arrayLength/3;
     }
 
+    // length    stacks   stacks' cap
+    //   0          0         0       
+    //   3          3         1       
+    //   6          3         2       
+    //   9          3         3       
     push(stackNum: number, value: T): void {
+        let from = stackNum* this.stackCapacity;
+        let to = from+ this.stackCapacity;
+        for(let i = from; i <= to ; i ++) {
+            if(!this.array[i]){
+                this.array[i] = value;
+                break; 
+            }
+        }
     }
 
     pop(stackNum: number): T | undefined {
+        let ele = undefined;
+        let from = stackNum* this.stackCapacity;
+        let to = from+ this.stackCapacity;
+        for(let i = to; i >= from ; i --) {
+            if(this.array[i]){
+                ele= this.array[i]
+                this.array[i] = undefined;
+                break; 
+            }
+        }
+        return ele;
     }
 
     peek(stackNum: number): T | undefined {
+        let ele = undefined;
+        let from = stackNum* this.stackCapacity;
+        let to = from+ this.stackCapacity;
+        for(let i = from; i <= to ; i ++) {
+            if(this.array[i]){
+                ele = this.array[i];
+                break; 
+            }
+        }
+        return ele
     }
 }
--- technical-fundamentals/coding/problems/22_stackMin.ts
+++ technical-fundamentals/coding/problems/22_stackMin.ts
@@ -5,19 +5,30 @@
 //
 
 export default class StackMin<T> {
+    private array: Array<T>;
+    private sortedArr: Array<T>;
     constructor() {
-
+        this.array = new Array();
+        this.sortedArr = new Array();
     }
 
     push(value: T): void {
-
+        this.array.push(value);
+        const lastMin = this.sortedArr[this.sortedArr.length -1]
+        if(!lastMin || (value < lastMin)) {
+            this.sortedArr.push(value)
+        }
     }
 
     pop(): T | undefined {
-
+        const ele = this.array.pop();
+        if (ele === this.sortedArr[this.sortedArr.length -1]){
+            this.sortedArr.pop();
+        }
+        return ele;
     }
 
     min(): T | undefined {
-
+        return this.sortedArr[this.sortedArr.length -1];
     }
 }
