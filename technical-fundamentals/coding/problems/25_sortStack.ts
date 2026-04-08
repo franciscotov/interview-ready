dd0a600c9554d69fe6596b016694168df1adba74
franciscodejesus.tovarceballo.contractor.gdevtools.com <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2026-04-08 11:43:33 -0300

    adding solution for sort and animal shelter stack

--- technical-fundamentals/coding/problems/25_sortStack.ts
+++ technical-fundamentals/coding/problems/25_sortStack.ts
@@ -6,18 +6,45 @@
 // The stack supports the following operations: push, pop, peek, and isEmpty.
 
 export default class SortStack<T> {
+    private stack: Array<T>;
     constructor() {
+        this.stack = new Array();
     }
 
     push(value: T): void {
+        let temp = [...this.stack]
+        const tempLength = temp.length;
+        let wasPushed: boolean = false;
+        this.stack = [];
+        if(tempLength === 0) {
+            this.stack.push(value);
+            return;
+        }
+        for(let i=0; i < tempLength; i++) {
+            let item = temp[i];
+            if(value < item && i === tempLength-1) {
+                this.stack.push(item);
+                this.stack.push(value);
+                
+            } else if(value > item && !wasPushed) {
+                wasPushed = true;
+                this.stack.push(value);
+                this.stack.push(item);
+            } else {
+                this.stack.push(item);
+            }
+        }
     }
 
     pop(): T | undefined {
+        return this.stack.pop();
     }
 
     peek(): T | undefined {
+        return this.stack[this.stack.length -1];
     }
 
     isEmpty(): boolean {
+        return this.stack.length === 0;
     }
 }
--- technical-fundamentals/coding/problems/26_animalShelter.ts
+++ technical-fundamentals/coding/problems/26_animalShelter.ts
@@ -9,6 +9,7 @@
 // Create the data structures to maintain this system and implement operations
 // such as enqueue, dequeueAny, dequeueDog, and dequeueCat.
 // You may use the built-in LinkedList data structure.
+import { LinkedList } from "./10_LinkedList";
 
 export type AnimalType = "dog" | "cat";
 
@@ -20,22 +21,56 @@ export class Animal {
 }
 
 export default class AnimalShelter {
+  private shelterList: LinkedList<Animal>;
+  constructor() {
+    this.shelterList = new LinkedList();
+  }
 
-    constructor() {
-    }
+  enqueue(type: AnimalType): void {
+    let animal = new Animal(type);
+    this.shelterList.push(animal);
+  }
 
-    enqueue(type: AnimalType): void {
+  dequeueAny(): Animal | undefined {
+    let res: Animal | undefined;
+    this.shelterList = this.shelterList.remove((node, i) => {
+      res = i === this.shelterList.length() -1 ? node.value: undefined;
+      return i !== this.shelterList.length() - 1;
+    });
+    return res;
+  }
 
-    }
+  dequeueDog(): Animal | undefined {
+    let res: Animal | undefined;
+    let lastDogIndex: number = 0;
+    this.shelterList.visit((node, i ) => {
+      if(node.value.type === 'dog') {
+        lastDogIndex = i;
+      }
+    })
+    this.shelterList = this.shelterList.remove((node, i) => {
+      if(node.value.type === 'dog' && i === lastDogIndex) {
+        res = node.value;
+      }
+      return i !== lastDogIndex;
+    });
+    return res;
+  }
 
-    dequeueAny(): Animal | undefined {
-
-    }
-
-    dequeueDog(): Animal | undefined {
-    }
-
-    dequeueCat(): Animal | undefined {
-    }
+  dequeueCat(): Animal | undefined {
+    let res: Animal | undefined;
+    let lastCatIndex: number = 0;
+    this.shelterList.visit((node, i ) => {
+      if(node.value.type === 'cat') {
+        lastCatIndex = i;
+      }
+    })
+    this.shelterList = this.shelterList.remove((node, i) => {
+      if(node.value.type === 'cat' && i === lastCatIndex) {
+        res = node.value;
+      }
+      return i !== lastCatIndex;
+    });
+    return res;
+  }
 }
-
