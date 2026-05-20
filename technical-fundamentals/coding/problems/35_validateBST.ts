32a07f269946c3ee44dc369693ee2eec6e4e5f02
franciscodejesus.tovarceballo.contractor.gdevtools.com <franciscodejesus.tovarceballo.contractor@gdevtools.com>
2026-05-20 19:11:32 -0300

    adding validateBST solution

--- technical-fundamentals/coding/problems/35_validateBST.ts
+++ technical-fundamentals/coding/problems/35_validateBST.ts
@@ -9,5 +9,50 @@ export type TreeNode<T> = {
 };
 
 export default function validateBST<T>(
-  node: TreeNode<T> | undefined,
-): boolean {}
+  node: TreeNode<T> | undefined
+): boolean {
+  /*
+  Invalid BST:
+           7
+          / \
+        4    8
+       / \
+      3   
+     / \
+    2   5
+  */
+  if (!node) {
+    return true;
+  }
+  // const stack = new Array<TreeNode<T>>();
+  // let pre: TreeNode<T> | undefined = undefined;
+  // while (node || !(stack.length === 0)) {
+  //   while (node) {
+  //     stack.push(node);
+  //     node = node.left;
+  //   }
+  //   node = stack.pop();
+  //   if (pre && node && node.value <= pre.value) return false;
+  //   pre = node;
+  //   node = node?.right;
+  // }
+  // return true;
+  // return recursive(node, undefined);
+  return recursive(node, []);
+}
+
+// recursive solution
+function recursive<T>(root: TreeNode<T> | undefined, results: Array<T>): boolean {
+  if(!root){
+    return true;
+  }
+  let isBSTleft = recursive(root.left, results);
+  let prev = results.pop();
+  if(prev != null && root.value <= prev){
+    results.push(root.value);
+    return false;
+  }
+  results.push(root.value);
+  let isBSTright = recursive(root.right, results);
+  return isBSTleft && isBSTright;
+}
